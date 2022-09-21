2754bda79cdb260c24d35bd30c30f4bf798df7cc
Fabio Nettis <nettisfabio@gmail.com>
2022-09-21 07:58:14 +0200

    feat: add logic for to new methods

--- src/classes/internals.ts
+++ src/classes/internals.ts
@@ -1,4 +1,11 @@
-import { UnifiedFormat } from '../types/query';
+import { injectorFactory } from '../utils/injections';
+
+import type {
+  Injection,
+  UnifiedFormat,
+  WithEntityID,
+  LocalizedResponse,
+} from '../types/query';
 
 class StencilInternals {
   private static instance: StencilInternals;
@@ -115,6 +122,59 @@ class StencilInternals {
   }
 
   /**
+   * This is the function that recursively localizes the response from the API
+   * by calling `localize` and thus itself on each property of the response if
+   * required.
+   *
+   * #### Notice
+   *
+   * Usually this function is called by the `localize` function, rather than calling this
+   * function directly, use the `localize` function.
+   *
+   * @param {Object} obj - Property of the response from the API
+   * @param {Boolean} component - If the response is a component
+   * @returns {Object} The reformatted response
+   */
+  localizeResponse<K extends string, T = any>(
+    sanitized: LocalizedResponse<T>,
+    inject?: Injection<K, T>,
+  ): Record<K, T> {
+    // If the response does not contain a localizations key, error
+    if (!sanitized?.localizations?.length) {
+      throw Error('No localizations found for this content type');
+    }
+
+    // If the response does not contain a locale key, error
+    if (!(sanitized as any)?.locale) {
+      throw Error('No locale found for this content type');
+    }
+
+    const localMap: Record<K, T> = {
+      [(sanitized as any).locale]: sanitized as T,
+    } as any;
+
+    for (const localization of sanitized.localizations) {
+      const { locale, ...rest } = localization as any;
+      (localMap as any)[locale as K] = { ...rest, locale } as any;
+    }
+
+    const injector: Injection<K, WithEntityID<T>> = injectorFactory<K, T>(
+      inject,
+    );
+
+    for (const key of Object.keys(localMap)) {
+      localMap[key as K] = injector(
+        localMap[key as K] as WithEntityID<T>,
+        sanitized as WithEntityID<LocalizedResponse<T>>,
+        key as K,
+      );
+    }
+
+    delete (localMap as any)[(sanitized as any).locale].localizations;
+    return localMap;
+  }
+
+  /**
    * This function is used to flatten any response from the API. The response
    * is usually a deeply nested object (unified response format). This function
    * will iterate over the object and remove all nesting.
@@ -157,6 +217,65 @@ class StencilInternals {
       return StencilInternals.getInstance().reformatResponse(item, isComponent);
     });
   }
+
+  i18n = {
+    /**
+     * The `i18n.t` function is used to localize a single or single response object from
+     * the API. It will iterate over the response and localize all strings, returning a
+     * new object with the locale as key and the flattened response as value.
+     *
+     * ### Keep in mind
+     *
+     * When using this function, that the response is not always in the
+     * unified response format, like in the users-permissions plugin, if no data key
+     * is found in the response the function will throw.
+     *
+     * Also keep in mind that this function will only localize the response if the
+     * locale key is present in the main entity and for each localization.
+     *
+     * @param {Object} response - The API response to localize
+     * @returns The localized entry
+     */
+    single<
+      K extends string = string,
+      T extends Record<any, any> = Record<any, any>,
+    >(response: any, inject?: Injection<K, T>): Record<K, T> {
+      const instance = StencilInternals.getInstance();
+      const sanitized: LocalizedResponse<T> = instance.flatten(response);
+      return instance.localizeResponse<K, T>(sanitized, inject);
+    },
+
+    /**
+     * The `i18n.array` function is used to localize a array response object from the API. It
+     * will iterate over the response and localize the content type, returning a new object
+     * with the locale as key and the flattened response as value.
+     *
+     * ### Keep in mind
+     *
+     * When using this function, that the response is not always in the
+     * unified response format, like in the users-permissions plugin, if no data key
+     * is found in the response the function will throw.
+     *
+     * Also keep in mind that this function will only localize the response if the
+     * locale key is present in the main entity and for each localization.
+     *
+     * @param {Object} response - The API response to localize
+     * @returns The localized entry
+     */
+    array<
+      K extends string = string,
+      T extends Record<any, any> = Record<any, any>,
+    >(response: any, inject?: Injection<K, T>): Array<Record<K, T>> {
+      const instance = StencilInternals.getInstance();
+      const sanitized: LocalizedResponse<T> = instance.flatten(response);
+      return sanitized.map((item: any) => {
+        return instance.localizeResponse<K, T>(
+          item as LocalizedResponse<T>,
+          inject,
+        );
+      });
+    },
+  };
 }
 
 export default StencilInternals.getInstance();
